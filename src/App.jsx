14a26a41429159dcf82af59612518f65bcd0a35d
masanarih0ri <masanarihoridevelop@gmail.com>
2020-05-09 17:40:51 +0900

    ルーティングを追加してURLごとにコンポーネントを表示する
    React, Vue, AngularのURLごとに内容表示を切り替えるためにRouteタグやBrowserRouterを利用して実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import Booklist from './components/Booklist';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   const languages = ['React', 'Vue', 'Angular'];
   return (
-    <div className="App">
-      <h1>hello world</h1>
-      <Booklist language={languages[0]} />
-      <Booklist language={languages[1]} />
-      <Booklist language={languages[2]} />
-    </div>
+    // BrowserRouterが必要
+    // exact path="/"は完全一致しているものだけを指定する。
+    <BrowserRouter>
+      <div className="App">
+        <h1>book app</h1>
+        <ul>
+          <li>
+            <Link to="/">React</Link>
+          </li>
+          <li>
+            <Link to="/vue">Vue</Link>
+          </li>
+          <li>
+            <Link to="/angular">Angular</Link>
+          </li>
+        </ul>
+        {/* ここのlangageは子コンポーネントに渡す際の名前 */}
+        <Route
+          exact
+          path="/"
+          render={(props) => <Booklist language={languages[0]} />}
+        />
+        <Route
+          path="/vue"
+          render={(props) => <Booklist language={languages[1]} />}
+        />
+        <Route
+          path="/angular"
+          render={(props) => <Booklist language={languages[2]} />}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
